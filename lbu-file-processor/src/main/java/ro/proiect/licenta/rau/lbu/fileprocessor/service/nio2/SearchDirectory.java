a11d66205519937d10943a852d2d9d09ebcab098
Raul Ciurescu <ciurescuraul@gmail.com>
2021-04-22 17:12:19 +0300

    [Test]: searching directory for oldest files to process

---
+++ lbu-file-processor/src/main/java/ro/proiect/licenta/rau/lbu/fileprocessor/service/nio2/SearchDirectory.java
@@ -0,0 +1,38 @@
+package ro.proiect.licenta.rau.lbu.fileprocessor.service.nio2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SearchDirectory
+{
+
+  public static void main(String[] args)
+  {
+
+    List<Path> pathList = new ArrayList<>();
+
+    try
+    {
+      pathList = Files.list(Paths.get("data"))
+          .filter(p -> !Files.isDirectory(p))
+          .filter(p -> p.toString().endsWith(".cdr"))
+          .filter(Files::isRegularFile)
+          .sorted(Comparator.comparingLong(p -> p.toFile().lastModified()))
+          .map(Path::toAbsolutePath).collect(Collectors.toList());
+
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+
+    pathList.forEach(System.out::println);
+
+  }
+}
