54f8095a7ddafb03cd8ca07a72f4446bfbb7fbe7
Raul Ciurescu <ciurescuraul@gmail.com>
2021-04-23 15:11:11 +0300

    [Main]: TODO list updated

--- lbu-file-processor/src/main/java/ro/proiect/licenta/rau/lbu/fileprocessor/VoiceCallFileProcessor.java
+++ lbu-file-processor/src/main/java/ro/proiect/licenta/rau/lbu/fileprocessor/VoiceCallFileProcessor.java
@@ -24,21 +24,21 @@ public class VoiceCallFileProcessor extends FileProcessor
     logger.info("Going to process file: '{}'", file.getFileName());
 
     // TODO: implement this part
-    // open the file
-    // read the file record by record
-    // process each record
-    // use a RecordProcessor similar with FileProcessor
-    // parse, validate, create corresponding VoiceCallDetails object and enqueue
-    // it
+    // - open the file DONE
+    // - read the file record by record DONE
+    // - process each record DONE
+    // - use a RecordProcessor similar with FileProcessor DONE
+    // - parse, validate  DONE
+    // create corresponding VoiceCallDetails object and enqueue it
     // write unit tests for record validation procedures
-    // - if record validation fails, write the record into an .error file
-    // - if enqueue fails (ActiveMQ is down), repeat unlimited with a one second
+    // if record validation fails, write the record into an .error file
+    // if enqueue fails (ActiveMQ is down), repeat unlimited with a one second
     // sleep (configurable) between retries
-    // - update file statistics for each record in memory
-    // - close the file
-    // - write file statistics into DB
-    // - if writing in DB fails, log an error and continue
-    // - rename the file (append .done extension to the file)
+    // update file statistics for each record in memory
+    // close the file
+    // write file statistics into DB
+    // if writing in DB fails, log an error and continue
+    // rename the file (append .done extension to the file)
 
     List<String> records = null;
 
